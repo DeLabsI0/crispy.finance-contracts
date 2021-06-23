633918677ea4a354b81177218306415287f468c7
Philippe Dumoet <philippe@dumo.net>
2021-06-23 09:00:01 +0200

    WIP

--- contracts/products/hex/HexStakeTokenizer.sol
+++ contracts/products/hex/HexStakeTokenizer.sol
@@ -45,10 +45,8 @@ contract HexStakeTokenizer is ERC721, FeeTaker {
             realTotal += stakeAmount;
             _stakeFor(_recipient, stakeAmount, _stakeDays[i]);
         }
-        uint256 feeToTake = realTotal * fee / (SCALE - fee);
-        uint256 stakeCost = realTotal + feeToTake;
+        uint256 stakeCost = _takeFeeForTotal(realTotal, hexToken);
         require(_upfrontTotal >= stakeCost, "CHXS: Insufficient funds");
-        _accountFee(hexToken, feeToTake);
         unchecked {
             uint256 refundAmount = _upfrontTotal - stakeCost;
             if (refundAmount > 0) hexToken.safeTransfer(msg.sender, refundAmount);
@@ -65,7 +63,7 @@ contract HexStakeTokenizer is ERC721, FeeTaker {
     {
         _checkFeeAtMost(_maxFee);
         hexToken.safeTransferFrom(msg.sender, address(this), _totalAmount);
-        uint256 stakeAmount = _takeFee(hexToken, _totalAmount);
+        uint256 stakeAmount = _takeFeeFrom(_totalAmount, hexToken);
         _stakeFor(_recipient, stakeAmount, _stakeDays);
     }
 
@@ -79,15 +77,32 @@ contract HexStakeTokenizer is ERC721, FeeTaker {
         uint256 totalUnstakedAmount;
         for (uint256 i; i < _tokenIds.length; i++) {
             uint256 tokenId = _tokenIds[i];
-            uint256 stakeIndex = _tokenIdToStakeIndex.get(tokenId);
-            totalUnstakedAmount += _unstake(tokenId, stakeIndex);
+            totalUnstakedAmount += _unstake(tokenId);
         }
         hexToken.safeTransfer(_recipient, totalUnstakedAmount);
     }
 
     function unstakeTo(address _recipient, uint256 _tokenId) external {
-        uint256 stakeIndex = _tokenIdToStakeIndex.get(_tokenId);
+        uint256 stakeIndex = getStakeIndex(_tokenId);
         _unstakeTo(_recipient, _tokenId, stakeIndex);
+    }
+
+    function extendStakeLength(
+        uint256 _tokenId,
+        uint256 _stakeDays,
+        uint256 _maxFee,
+        uint256 _addedAmount
+    )
+        external
+    {
+        _checkFeeAtMost(_maxFee);
+        _checkHasAccessTo(_tokenId);
+        uint256 newStakeAmount = _unstake(_tokenId);
+        if (_addedAmount > 0) {
+            hexToken.safeTransferFrom(msg.sender, address(this), _addedAmount);
+            newStakeAmount += _addedAmount;
+        }
+        newStakeIndex = totalSupply - 1;
     }
 
     /* should only be used if there is a bug in the sc and `unstakeTo` no longer
@@ -112,6 +127,14 @@ contract HexStakeTokenizer is ERC721, FeeTaker {
 
     function getTokenStakeId(uint256 _tokenId) public view returns (uint256) {
         return _stakeIdOfToken[_tokenId];
+    }
+
+    function _checkHasAccessTo(uint256 _tokenId) internal view {
+        address owner = ownerOf(_tokenId);
+        require(
+            msg.sender == owner || isApprovedForAll(owner, msg.sender),
+            "CHXS: Not token owner"
+        );
     }
 
     function _stakeFor(
@@ -140,10 +163,15 @@ contract HexStakeTokenizer is ERC721, FeeTaker {
         hexToken.safeTransfer(_recipient, unstakedAmount);
     }
 
+    function _unstake(uint256 _tokenId) internal returns (uint256) {
+        uint256 stakeIndex = getStakeIndex(_tokenId);
+        return _unstake(_tokenId, stakeIndex);
+    }
+
     function _unstake(uint256 _tokenId, uint256 _stakeIndex)
         internal returns (uint256 unstakedAmount)
     {
-        require(ownerOf(_tokenId) == msg.sender, "CHXS: Not token owner");
+        _checkHasAccessTo(_tokenId);
         uint256 stakeId = _verifyTokenStakeIndex(_tokenId, _stakeIndex);
         unstakedAmount = _endStake(_tokenId, _stakeIndex, stakeId);
     }
@@ -158,9 +186,9 @@ contract HexStakeTokenizer is ERC721, FeeTaker {
         _burn(_tokenId);
 
         // if it wasn't the last stake in the list something got rearanged
-        uint256 totalSupply_ = totalSupply;
-        if (_stakeIndex != totalSupply_) {
-            uint256 topTokenId = _tokenIdToStakeIndex.rget(totalSupply_);
+        uint256 lastStakeIndex = totalSupply;
+        if (_stakeIndex != lastStakeIndex) {
+            uint256 topTokenId = getTokenId(lastStakeIndex);
             _tokenIdToStakeIndex.set(topTokenId, _stakeIndex);
         }
 
